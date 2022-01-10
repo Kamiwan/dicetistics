502d29a78b6643f10fca2d60b3ff535d32f5868d
Kamiwan <erwan.moreac@gmail.com>
2022-01-10 22:28:51 +0100

    Add to use dice with custom faces

--- custom_rolls.py
+++ custom_rolls.py
@@ -6,10 +6,10 @@ from matplotlib.ticker import MaxNLocator
 from dice import Dice
 
 class CustomRoll:
-    def __init__(self, dice_nb, face_nb, roll_nb):
+    def __init__(self, dice_nb, dice, roll_nb):
         self.dice_nb = dice_nb
         self.roll_nb = roll_nb
-        self.dice = Dice(face_nb)
+        self.dice = dice
         self.raw_result = 0
 
     def roll_all(self):
@@ -23,7 +23,10 @@ def main():
     dice_nb = int(input("How many dice to roll in the same time: "))
     rolls = int(input("How many times do you want to repeat the roll: "))
 
-    test = CustomRoll(dice_nb, face, rolls)
+    custom_faces = np.array([0,0,0,0,1,1])
+    custom_dice = Dice(face, custom_faces)
+    max_face = np.max(custom_faces)
+    test = CustomRoll(dice_nb, custom_dice, rolls)
     test.roll_all()
 
     # plot
@@ -34,11 +37,15 @@ def main():
     print("moyenne = " , moy)
     print("ecart type = " , std)
 
+    print(x)
+    print(np.max(x))
+
     fig, ax = plt.subplots()
-    ax.hist(x, bins=np.arange(1,face*dice_nb+2)) # linewidth=0.5, edgecolor="white"
-    ax.set(xlim=(1, face*dice_nb+1),xticks=np.arange(1, face*dice_nb+1))
+    ax.hist(x, bins=np.arange(0,face*dice_nb+2)) # linewidth=0.5, edgecolor="white"
+    ax.set(xlim=(0, max_face*dice_nb+1),xticks=np.arange(0, face*dice_nb+1))
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     title = "Histograms for rolling " + str(dice_nb) + " dice of " + str(face) + " faces " + str(rolls) + " times."
+    title = title + "\n dice faces = " + str(custom_faces)
     plt.title(title, loc='center')
     plt.xlabel('Roll value')
     plt.ylabel('Amount of roll')
@@ -50,9 +57,9 @@ def main():
     mean_str = "mean = " + str(np.around(moy, 3))
     std_str = "$\sigma$ = " + str(np.around(std, 3))
     gap_str = str(int(moy-(std*2))) + "< 95% of values < " + str(int(moy+(std*2)))
-    plt.text(1, top*0.9, mean_str, size=15, color='purple')
-    plt.text(1, top*0.8, std_str, size=15, color='purple')
-    plt.text(1, top*0.7, gap_str, size=10, color='purple')
+    plt.text(0, top*0.9, mean_str, size=15, color='purple')
+    plt.text(0, top*0.8, std_str, size=15, color='purple')
+    plt.text(0, top*0.7, gap_str, size=10, color='purple')
 
 
     plt.show()
--- dice.py
+++ dice.py
@@ -3,27 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Dice:
-    def __init__(self, face_nb):
+    def __init__(self, face_nb, custom_faces=None):
         self.face_nb = face_nb
         self.last_roll = 0
+        if np.any(custom_faces == None) :
+            self.custom_faces = np.arange(10, dtype=int)
+        else:
+            self.custom_faces = custom_faces
 
     def roll(self, nb_rolls):
-        self.last_roll = np.random.randint(low=1, high=self.face_nb+1, size=nb_rolls)
+        self.last_roll = self.custom_faces[np.random.randint(low=0, high=self.face_nb, size=nb_rolls)]
         return self.last_roll
+
+    def set_custom_faces(self, custom_faces):
+        self.custom_faces = custom_faces
+
+    def get_custom_faces(self):
+        return self.custom_faces
 
 
 def main():
     print("=== Dicestistics tool! ===")
     face = int(input("How many faces for you dice: "))
     rolls = int(input("How many times do you want to roll the dice: "))
-    a = Dice(face)
+    custom_dice = np.array([0,0,0,0,1,1])
+    a = Dice(face, custom_dice)
     a.roll(rolls)
     
     # plot
     x = a.last_roll
     fig, ax = plt.subplots()
-    ax.hist(x, bins=np.arange(1,face+2), linewidth=0.5, edgecolor="white")
-    ax.set(xlim=(1, face+1),xticks=np.arange(1, face+1))
+    ax.hist(x, bins=np.arange(0,face+2), linewidth=0.5, edgecolor="white")
+    ax.set(xlim=(0, face+1),xticks=np.arange(0, face+1))
     plt.show()
  
 if __name__ == "__main__":
